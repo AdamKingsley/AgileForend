3b59281c21a829d514ff44bda0d92ff70462cb4e
AdamKingsley <desperatewilder@gmail.com>
2019-01-15 15:29:33 +0800

    选择城市前端样式

--- pages/city_selector/city_selector.js
+++ pages/city_selector/city_selector.js
@@ -5,122 +5,177 @@ Page({
   data: {
     inputShowed: false,
     select_text: "",
-    list: [
-      {
-        listName: '列表1',
-        item: [{
-          itemName: '子列表1-1',
-          content: '1-1中的内容',
-          time: '2015-05-06'
-        }, {
-          itemName: '子列表1-2',
-          content: '1-2中的内容',
-          time: '2015-04-13'
-        }, {
-          itemName: '子列表1-3',
-          content: '1-3中的内容',
-          time: '2015-12-06'
-        }]
+    select_province: {
+      id: 1,
+      itemName: '江苏省'
+    },
+    select_city: {
+      id: 1,
+      itemName: '南京'
+    },
+    province_list_visiable: false,
+    province_list: [{
+        id: 1,
+        itemName: '北京市',
+        content: '',
+      }, {
+        id: 2,
+        itemName: '江苏省',
+        content: '',
       },
       {
-        listName: '列表2',
-        item: [{
-          itemName: '子列表2-1',
-          content: '2-1中的内容',
-          time: '2017-05-06'
-        }, {
-          itemName: '子列表2-2',
-          content: '2-2中的内容',
-          time: '2015-08-06'
-        }, {
-          itemName: '子列表2-3',
-          content: '2-3中的内容',
-          time: '2015-11-06'
-        }]
-      }, {
-        listName: '列表3',
-        item: [{
-          itemName: '子列表3-1',
-          content: '3-1中的内容',
-          time: '2015-05-15'
-        }, {
-          itemName: '子列表3-2',
-          content: '3-2中的内容',
-          time: '2015-05-24'
-        }, {
-          itemName: '子列表1-3',
-          content: '3-3中的内容',
-          time: '2015-05-30'
-        }]
+        id: 3,
+        itemName: '天津市',
+        content: '',
+      },
+      {
+        id: 4,
+        itemName: '重庆市',
+        content: '',
+      },
+      {
+        id: 5,
+        itemName: '上海市',
+        content: '',
+      },
+      {
+        id: 6,
+        itemName: '山东省',
+        content: '',
       }
-    ]
+    ],
+    city_list: [{
+      id: 1,
+      itemName: '南京',
+      content: '',
+    }, {
+      id: 2,
+      itemName: '苏州',
+      content: '',
+    }, {
+      id: 3,
+      itemName: '无锡',
+      content: '',
+    }, {
+      id: 4,
+      itemName: '常州',
+      content: '',
+    }, {
+      id: 5,
+      itemName: '扬州',
+      content: '',
+    }, {
+      id: 6,
+      itemName: '泰州',
+      content: '',
+    }, {
+      id: 7,
+      itemName: '宿迁',
+      content: '',
+    }, {
+      id: 8,
+      itemName: '济南',
+      content: '',
+    }, {
+      id: 9,
+      itemName: '烟台',
+      content: '',
+    }, {
+      id: 10,
+      itemName: '临沂',
+      content: '',
+    }]
   },
 
   //点击最外层列表展开收起
   listTap(e) {
     console.log('触发了最外层');
-    let Index = e.currentTarget.dataset.parentindex,//获取点击的下标值
-      list = this.data.list;
-    list[Index].show = !list[Index].show || false;//变换其打开、关闭的状态
-    if (list[Index].show) {//如果点击后是展开状态，则让其他已经展开的列表变为收起状态
-      this.packUp(list, Index);
-    }
-
+    // let Index = e.currentTarget.dataset.parentindex, //获取点击的下标值
+    // visiable = this.data.province_list_visiable;
+    // visiable = !visiable || false; 
+    //变换其打开、关闭的状态;
     this.setData({
-      list
+      province_list_visiable: !this.data.province_list_visiable || false
+    })
+  },
+  select_province(e) {
+    console.log(e);
+    this.listTap(e);
+    this.setData({
+      select_province: {
+        id: e.currentTarget.dataset.index,
+        itemName: e.currentTarget.dataset.name
+      }
     });
+    //选择省份之后应该向后端请求该省份包含的城市
+    //然后展示城市列表
   },
-  //点击里面的子列表展开收起
-  listItemTap(e) {
-    let parentindex = e.currentTarget.dataset.parentindex,//点击的内层所在的最外层列表下标
-      Index = e.currentTarget.dataset.index,//点击的内层下标
-      list = this.data.list;
-    console.log(list[parentindex].item, Index);
-    list[parentindex].item[Index].show = !list[parentindex].item[Index].show || false;//变换其打开、关闭的状态
-    if (list[parentindex].item[Index].show) {//如果是操作的打开状态，那么就让同级的其他列表变为关闭状态，保持始终只有一个打开
-      for (let i = 0, len = list[parentindex].item.length; i < len; i++) {
-        if (i != Index) {
-          list[parentindex].item[i].show = false;
-        }
-
+  select_city(e) {
+    //选择城市
+    console.log(e);
+    console.log(e.currentTarget.dataset.index, e.currentTarget.dataset.name);
+    this.setData({
+      city: {
+        id: e.currentTarget.dataset.index,
+        itemName: e.currentTarget.dataset.name,
       }
-    }
-    this.setData({ list });
-  },
-  //让所有的展开项，都变为收起
-  packUp(data, index) {
-    for (let i = 0, len = data.length; i < len; i++) {//其他最外层列表变为关闭状态
-      if (index != i) {
-        data[i].show = false;
-        for (let j = 0; j < data[i].item.length; j++) {//其他所有内层也为关闭状态
-          data[i].item[j].show = false;
-        }
-      }
-    }
+    });
+    app.globalData.city = {
+      id: e.currentTarget.dataset.index,
+      itemName: e.currentTarget.dataset.name
+    };
+    app.globalData.province = {
+      id: this.data.select_province.id,
+      itemName: this.data.select_province.itemName
+    };
+    // console.log(app.globalData)
+    wx.switchTab({
+      url: '../index/index',
+      success: function(res) {
+        console.log(res);
+        console.log("成功跳转！");
+      },
+      fail: function(res) {
+        console.log(res)
+        console.log("跳转失败！")
+      },
+      complete: function(res) {},
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.log(options);
+    //初始化首页中固定选择的城市和省份
+    this.setData({
+      select_province: {
+        id: options.provinceId ? options.provinceId : this.data.select_province.id,
+        itemName: options.province ? options.province : this.data.select_province.itemName,
+      },
+      select_city: {
+        id: options.cityId ? options.cityId : this.data.select_city.id,
+        itemName: options.city ? options.city : this.data.select_city.itemName,
+      }
+    });
   },
-  showInput: function () {
+  showInput: function() {
     this.setData({
       inputShowed: true
     });
   },
-  hideInput: function () {
+  hideInput: function() {
     this.setData({
       select_text: "",
       inputShowed: false
     });
   },
-  clearInput: function () {
+  clearInput: function() {
     this.setData({
       select_text: ""
     });
   },
-  inputTyping: function (e) {
+  inputTyping: function(e) {
     this.setData({
       select_text: e.detail.value
     });
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,33 +5,52 @@ Page({
   data: {
     inputShowed: false,
     select_text: "",
-    city:'南京'
+    province: '江苏省',
+    provinceId: 2,
+    city: '南京',
+    cityId: 1,
   },
-  selectCity: function(){
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log('index onload');
+    console.log(app.globalData)
+    this.setData({
+      city: app.globalData.city ? app.globalData.city.itemName : this.data.city,
+      cityId: app.globalData.city ? app.globalData.city.id : this.data.cityId,
+      province: app.globalData.province ? app.globalData.province.itemName : this.data.province,
+      provinceId: app.globalData.province ? app.globalData.province.id : this.data.provinceId,
+    });
+  },
+  onShow: function(options) {
+    this.onLoad(options);
+  },
+  selectCity: function() {
     wx.navigateTo({
-      url: '../city_selector/city_selector',
+      url: '../city_selector/city_selector?cityId=' + this.data.cityId + '&city=' + this.data.city + '&province=' + this.data.province + '&provinceId=' + this.data.provinceId,
       success: function(res) {},
       fail: function(res) {},
       complete: function(res) {},
     })
   },
-  showInput: function () {
+  showInput: function() {
     this.setData({
       inputShowed: true
     });
   },
-  hideInput: function () {
+  hideInput: function() {
     this.setData({
       select_text: "",
       inputShowed: false
     });
   },
-  clearInput: function () {
+  clearInput: function() {
     this.setData({
       select_text: ""
     });
   },
-  inputTyping: function (e) {
+  inputTyping: function(e) {
     this.setData({
       select_text: e.detail.value
     });
