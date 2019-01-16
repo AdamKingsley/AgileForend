8c74fe3644aedf344e36cf90cac46bac882d5dc1
AdamKingsley <desperatewilder@gmail.com>
2019-01-16 10:15:59 +0800

    首页样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,62 @@ Page({
     provinceId: 2,
     city: '南京',
     cityId: 1,
-    sights: [{id:1,}, {id:2,}, {id:3,}, {id:4,}, {id:5,}, {id:6,}, {id:7,}, {id:8,}]
+    sights: [{
+      id: 1,
+      pic: '../../images/sight_template.jpg',
+      name: "总统府",
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 4.7,
+      price: 20,
+      labels:['4A景区','名楼','俯瞰长江']
+    }, {
+      id: 2,
+      name: '夫子庙',
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 3.2,
+      price: 20,
+    }, {
+      id: 3,
+      name: '老门东',
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 5.0,
+      price: 20,
+    }, {
+      id: 4,
+      name: "狮子桥",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 4.7,
+      price: 0,
+    }, {
+      id: 5,
+      name: "中山陵",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 1.7,
+    }, {
+      id: 6,
+      name: "雨花台",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 3.7,
+    }, {
+      id: 7,
+      pic: '../../images/sight_template.jpg',
+      name: "燕子矶",
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 5.0,
+      price: 150,
+    }, {
+      id: 8,
+      name: "阅江楼",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 5.0,
+      price: 100,
+    }]
   },
   /**
    * 生命周期函数--监听页面加载
