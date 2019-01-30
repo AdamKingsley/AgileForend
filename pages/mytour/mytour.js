48adcdf4f42e5bba88403f061674d06b3037c3c6
unknown <1330486645@qq.com>
2019-01-30 21:15:18 +0800

    添加个人主页相关页面

--- pages/mytour/mytour.js
+++ pages/mytour/mytour.js
@@ -1,66 +1,114 @@
-// pages/mytour/mytour.js
+//mytour 我的出游
+var util = require('../../utils/util.js')
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    inputShowed: false,
+    select_text: "",
+    province: '江苏省',
+    provinceId: '320000',
+    city: '南京市',
+    cityId: '320100',
+    sights: [{
+      id: 1,
+      pic: 'http://localhost:9999/pics/sight/sight_template.jpg',
+      name: "总统府",
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 4.7,
+      price: 20,
+      labels: ['4A景区', '名楼', '俯瞰长江']
+    }, {
+      id: 2,
+      name: '夫子庙',
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 3.2,
+      price: 20,
+    }, {
+      id: 3,
+      name: '老门东',
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 5.0,
+      price: 20,
+    }, {
+      id: 4,
+      name: "狮子桥",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 4.7,
+      price: 0,
+    }, {
+      id: 5,
+      name: "中山陵",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 1.7,
+    }, {
+      id: 6,
+      name: "雨花台",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 3.7,
+    }, {
+      id: 7,
+      pic: '../../images/sight_template.jpg',
+      name: "燕子矶",
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 5.0,
+      price: 150,
+    }, {
+      id: 8,
+      name: "阅江楼",
+      pic: '../../images/sight_template.jpg',
+      description: "这里美如画，这里贼好玩，这里的山路十八弯，这里的水路九连环",
+      score: 5.0,
+      price: 100,
+    }]
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('mytour onload');
+    console.log(app.globalData)
+    this.setData({
+      city: app.globalData.city ? app.globalData.city.itemName : this.data.city,
+      cityId: app.globalData.city ? app.globalData.city.id : this.data.cityId,
+      province: app.globalData.province ? app.globalData.province.itemName : this.data.province,
+      provinceId: app.globalData.province ? app.globalData.province.id : this.data.provinceId,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow: function (options) {
+    this.onLoad(options);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // selectCity: function () {
+  //   wx.navigateTo({
+  //     url: '../city_selector/city_selector?cityId=' + this.data.cityId + '&city=' + this.data.city + '&province=' + this.data.province + '&provinceId=' + this.data.provinceId,
+  //     success: function (res) { },
+  //     fail: function (res) { },
+  //     complete: function (res) { },
+  //   })
+  // },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  hideInput: function () {
+    this.setData({
+      select_text: "",
+      inputShowed: false
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  clearInput: function () {
+    this.setData({
+      select_text: ""
+    });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  inputTyping: function (e) {
+    this.setData({
+      select_text: e.detail.value
+    });
   }
 })
