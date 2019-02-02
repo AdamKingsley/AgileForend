2e3c32fa75fbdb427db6380cde70228d6fa5f616
Christine <njuzsq@outlook.com>
2019-02-03 00:56:03 +0800

    create tour

--- pages/tour/tour.js
+++ pages/tour/tour.js
@@ -159,5 +159,12 @@ Page({
     this.setData({
       select_text: e.detail.value
     });
+  },
+
+  goToCreateTour: function (parm) {
+    console.log('a');
+    wx.navigateTo({
+      url: '/pages/create_tour/create_tour',
+    })
   }
 })
