643721a990ad719f919cc7ecba6cd818ec66c29f
AdamKingsley <desperatewilder@gmail.com>
2019-02-03 01:15:43 +0800

    首页+详情+登录基本完成

---
+++ pages/sight_detail/data.js
@@ -0,0 +1,12 @@
+let swiper_config = {
+  indicatorDots: true, //是否显示面板指示点
+  autoplay: true, //是否自动切换
+  interval: 3000, //自动切换时间间隔
+  duration: 1000, //滑动动画时长
+  inputShowed: false,
+  inputVal: ""
+}
+
+module.exports = {
+  swiper_config
+};
